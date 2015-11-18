b5eafd650e0d97dec64920e8cd90fc7cf9bb984d
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2015-11-18 01:12:50 +0100

    First commit

---
+++ js/mygmain.js
@@ -0,0 +1,108 @@
+/* global THREE */
+var camera, scene, renderer;
+var cameraOrtho, sceneOrtho;
+
+var spriteTL, spriteTR, spriteBL, spriteBR, spriteC;
+var attackSprite, defendSprite, itemSprite, runSprite;
+
+//var canvas = document.getElementById("mycanvas");
+var rendererWidth = 800;
+var rendererHeight = 450;
+
+init();
+animate();
+
+function init() {
+	//Scene setting
+	camera = new THREE.PerspectiveCamera( 60, rendererWidth / rendererHeight, 1, 2100 );
+	camera.position.z = 150;
+
+	cameraOrtho = new THREE.OrthographicCamera( - rendererWidth / 2, rendererWidth / 2, rendererHeight / 2, - rendererHeight / 2, 1, 10);
+	cameraOrtho.position.z = 10;
+
+	scene = new THREE.Scene();
+	scene.fog = new THREE.Fog( 0x000000, 1500, 2100 );
+
+	sceneOrtho = new THREE.Scene();
+	
+	var loader = new THREE.TextureLoader();
+	
+	//Objects
+	var mapA = loader.load('/img/blue128.png', createHUDSprites);
+	//var mapA = THREE.ImageUtils.loadTexture( "img/blue128.png", undefined, createHUDSprites );
+	// renderer
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( rendererWidth, rendererHeight );
+	renderer.autoClear = false; // To allow render overlay on top of sprited sphere
+
+	document.body.appendChild( renderer.domElement );
+	
+}
+
+function createHUDSprites ( texture ) {
+	console.log("setting everything");
+	var material = new THREE.SpriteMaterial( { map: texture } );
+
+	var width = material.map.image.width;
+	var height = material.map.image.height;
+	
+	attackSprite = new THREE.Sprite( material );
+	attackSprite.scale.set( width, height, 1 );
+	sceneOrtho.add( attackSprite );
+
+	spriteTR = new THREE.Sprite( material );
+	spriteTR.scale.set( width, height, 1 );
+	sceneOrtho.add( spriteTR );
+
+	spriteBL = new THREE.Sprite( material );
+	spriteBL.scale.set( width, height, 1 );
+	sceneOrtho.add( spriteBL );
+
+	spriteBR = new THREE.Sprite( material );
+	spriteBR.scale.set( width, height, 1 );
+	sceneOrtho.add( spriteBR );
+	
+	spriteC = new THREE.Sprite( material );
+	spriteC.scale.set( width, height, 1 );
+	sceneOrtho.add( spriteC );
+
+	updateHUDSprites();
+
+}
+
+function updateHUDSprites () {
+
+	var width = rendererWidth / 2;
+	var height = rendererHeight / 2;
+
+	var material = spriteTR.material;
+	
+	console.log(material);
+
+	var imageWidth = material.map.image.width / 2;
+	var imageHeight = material.map.image.height / 2;
+
+	attackSprite.position.set( - width + imageWidth,   height - imageHeight, 1 ); // top left
+	spriteTR.position.set(   width - imageWidth,   height - imageHeight, 1 ); // top right
+	spriteBL.position.set( - width + imageWidth, - height + imageHeight, 1 ); // bottom left
+	spriteBR.position.set(   width - imageWidth, - height + imageHeight, 1 ); // bottom right
+	spriteC.position.set( 0, 0, 1 ); // center
+
+}
+
+function animate() {
+	requestAnimationFrame( animate );
+	render();
+}
+
+function render () {
+	var time = Date.now() / 1000;
+
+	renderer.clear();
+	renderer.render( scene, camera );
+	renderer.clearDepth();
+	renderer.render( sceneOrtho, cameraOrtho );
+}
+
---
+++ server.js
@@ -0,0 +1,58 @@
+var fs = require('fs');
+
+var express = require('express');
+var app = express();
+
+var ejs = require('ejs');
+
+var busboy = require('connect-busboy');
+
+var server = require('http').Server(app);
+
+var io = require('socket.io')(server);
+
+//MongoDB
+var MongoClient = require('mongodb').MongoClient;
+
+var assert = require('assert');
+
+// Render engine
+app.engine('html', ejs.renderFile);
+
+app.use(busboy());
+
+//View folder
+app.set('views', __dirname + '/views');
+
+app.use("/css", express.static( __dirname + '/css'));
+app.use("/img", express.static( __dirname + '/img'));
+app.use("/js", express.static( __dirname + '/js'));
+app.use("/videos", express.static( __dirname + '/videos'));
+
+app.get('/', function(req, res) {
+	res.render('index.ejs.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('message', function(msg){
+    console.log('mess: ' + msg);
+  });
+
+  socket.on('addVideo', function(videoResult){
+    var section = videoResult.sectionName;
+    var video = videoResult.video;
+    addVideo(section, video.videoSource, video.videoName, video.videoImage, video.videoImageOffset);
+  });
+
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, function(socket) {
+	console.log('listening on *:3000');
+});
+
